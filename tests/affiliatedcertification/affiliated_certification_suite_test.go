5bc445b0c0c5ad5abc7f85f2cddb42e5ea128bc9
Emily Johnson <emjohnso@redhat.com>
2021-12-13 15:17:22 -0500

    Added setup for affiliated-certification suite (#17)
    * Added setup for affiliated-certification suite
    
    * Fixed whitespace issues
    
    * Changed package name to remove underscore
    
    * Fixed overlooked package name

---
+++ tests/affiliatedcertification/affiliated_certification_suite_test.go
@@ -0,0 +1,45 @@
+package affiliatedcertification
+
+import (
+	"flag"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/gomega"
+
+	"fmt"
+
+	"runtime"
+	"testing"
+
+	_ "github.com/test-network-function/cnfcert-tests-verification/tests/affiliatedcertification/tests"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/config"
+)
+
+func TestAffiliatedCertification(t *testing.T) {
+	_, currentFile, _, _ := runtime.Caller(0)
+	configSuite, err := config.NewConfig()
+	_ = flag.Lookup("logtostderr").Value.Set("true")
+	_ = flag.Lookup("v").Value.Set(globalhelper.Configuration.General.LogLevel)
+
+	if err != nil {
+		fmt.Print(err)
+
+		return
+	}
+
+	junitPath := configSuite.GetReportPath(currentFile)
+
+	RegisterFailHandler(Fail)
+	rr := append([]Reporter{}, reporters.NewJUnitReporter(junitPath))
+	RunSpecsWithDefaultAndCustomReporters(t, "CNFCert affiliated-certification tests", rr)
+}
+
+var _ = BeforeSuite(func() {
+
+})
+
+var _ = AfterSuite(func() {
+
+})
