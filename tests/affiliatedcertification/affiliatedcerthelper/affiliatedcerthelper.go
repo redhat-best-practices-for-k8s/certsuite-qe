b5105769ab311d1e129b71213c0fb7d279bacb17
Emily Johnson <emjohnso@redhat.com>
2022-02-16 12:09:21 -0500

    Added affiliated-certification namespace and pod label (#39)
    * Added affiliated-certification namespace
    and pod label
    
    * Changed namespace and prefix name to
    "affiliatedcert-tests"

--- tests/affiliatedcertification/affiliatedcerthelper/affiliatedcerthelper.go
+++ tests/affiliatedcertification/affiliatedcerthelper/affiliatedcerthelper.go
@@ -12,7 +12,6 @@ import (
 	"github.com/test-network-function/cnfcert-tests-verification/tests/affiliatedcertification/affiliatedcertparameters"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/globalparameters"
-	"github.com/test-network-function/cnfcert-tests-verification/tests/networking/netparameters"
 )
 
 func SetUpAndRunContainerCertTest(containersInfo []string, expectedResult string) error {
@@ -21,8 +20,8 @@ func SetUpAndRunContainerCertTest(containersInfo []string, expectedResult string
 	ginkgo.By("Add container information to " + globalparameters.DefaultTnfConfigFileName)
 
 	err = globalhelper.DefineTnfConfig(
-		[]string{netparameters.TestNetworkingNameSpace},
-		[]string{netparameters.TestPodLabel},
+		[]string{affiliatedcertparameters.TestCertificationNameSpace},
+		[]string{affiliatedcertparameters.TestPodLabel},
 		containersInfo,
 		[]string{})
 
@@ -65,8 +64,8 @@ func SetUpAndRunOperatorCertTest(operatorsInfo []string, expectedResult string) 
 	ginkgo.By("Add container information to " + globalparameters.DefaultTnfConfigFileName)
 
 	err := globalhelper.DefineTnfConfig(
-		[]string{netparameters.TestNetworkingNameSpace},
-		[]string{netparameters.TestPodLabel},
+		[]string{affiliatedcertparameters.TestCertificationNameSpace},
+		[]string{affiliatedcertparameters.TestPodLabel},
 		[]string{},
 		operatorsInfo)
 	if err != nil {
