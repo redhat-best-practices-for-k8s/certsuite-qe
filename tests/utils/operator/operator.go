69ad8443f2852842656c231dcc4a9ca47168208f
Emily Johnson <emjohnso@redhat.com>
2022-02-23 14:24:11 -0500

    Added operator deployment infrastructure and example test case to test it

---
+++ tests/utils/operator/operator.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	olmv1 "github.com/operator-framework/api/pkg/operators/v1"
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func DefineOperatorGroup(groupName string, namespace string, targetNamespace []string) *olmv1.OperatorGroup {
+	return &olmv1.OperatorGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      groupName,
+			Namespace: namespace},
+		Spec: olmv1.OperatorGroupSpec{
+			TargetNamespaces: targetNamespace},
+	}
+}
+
+func DefineSubscription(subName, namespace, channel, operatorName, catalogSource,
+	catalogSourceNamespace string) *v1alpha1.Subscription {
+	return &v1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      subName,
+			Namespace: namespace,
+		},
+		Spec: &v1alpha1.SubscriptionSpec{
+			Channel:                channel,
+			Package:                operatorName,
+			CatalogSource:          catalogSource,
+			CatalogSourceNamespace: catalogSourceNamespace,
+		},
+	}
+}
