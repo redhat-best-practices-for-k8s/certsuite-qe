50f1f0f1d4f3c9e5c148e912ff3fd050291e3eb3
sobarzan <sobarzan@redhat.com>
2022-03-13 16:09:29 +0200

    Added node uncordoning verification to IsClusterStable for better stability

--- tests/utils/cluster/cluster.go
+++ tests/utils/cluster/cluster.go
@@ -9,16 +9,24 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	testclient "github.com/test-network-function/cnfcert-tests-verification/tests/utils/client"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/nodes"
 )
 
 func IsClusterStable(clients *testclient.ClientSet) (bool, error) {
-	nodes, err := clients.Nodes().List(context.Background(), metav1.ListOptions{})
+	nodeInCluster, err := clients.Nodes().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return false, err
 	}
 
-	for _, node := range nodes.Items {
+	for _, node := range nodeInCluster.Items {
 		if node.Spec.Unschedulable {
+			fmt.Println("node", node.Name, "is cordoned, uncordoning it")
+			_, err := nodes.CordonOrUnCordonNodeByName(node.Name, clients, false)
+
+			if err != nil {
+				return false, err
+			}
+
 			glog.V(5).Info(fmt.Sprintf("node %s is in unschedulable state", node.Name))
 
 			return false, nil
--- tests/utils/nodes/nodes.go
+++ tests/utils/nodes/nodes.go
@@ -2,14 +2,22 @@ package nodes
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"github.com/golang/glog"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/client"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
+
+type patchStringValue struct {
+	Operation string `json:"op"`
+	Path      string `json:"path"`
+	Value     bool   `json:"value"`
+}
 
 // WaitForNodesReady waits for all nodes become ready.
 func WaitForNodesReady(cs *client.ClientSet, timeout, interval time.Duration) error {
@@ -56,4 +64,23 @@ func GetNumOfReadyNodesInCluster(cs *client.ClientSet) (int32, error) {
 	}
 
 	return int32(numOfNodesExistsInCluster), nil
+}
+
+// CordonOrUnCordonNodeByName cordones/uncordones a node by a given node name.
+func CordonOrUnCordonNodeByName(nodeName string, clients *client.ClientSet, cordon bool) (bool, error) {
+	payload := []patchStringValue{{
+		Operation: "replace",
+		Path:      "/spec/unschedulable",
+		Value:     cordon,
+	}}
+	payloadBytes, _ := json.Marshal(payload)
+
+	_, err := clients.Nodes().Patch(context.Background(), nodeName, types.JSONPatchType,
+		payloadBytes, metav1.PatchOptions{})
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
