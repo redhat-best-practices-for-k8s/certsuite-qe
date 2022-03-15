82854f69659934d1b72f1ae73f986e02b9141722
sobarzan <sobarzan@redhat.com>
2022-03-15 07:59:31 +0200

    Fixed var name in IsClusterStable

--- tests/utils/cluster/cluster.go
+++ tests/utils/cluster/cluster.go
@@ -9,19 +9,19 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	testclient "github.com/test-network-function/cnfcert-tests-verification/tests/utils/client"
-	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/nodes"
+	nodesutils "github.com/test-network-function/cnfcert-tests-verification/tests/utils/nodes"
 )
 
 func IsClusterStable(clients *testclient.ClientSet) (bool, error) {
-	nodeInCluster, err := clients.Nodes().List(context.Background(), metav1.ListOptions{})
+	nodes, err := clients.Nodes().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return false, err
 	}
 
-	for _, node := range nodeInCluster.Items {
+	for _, node := range nodes.Items {
 		if node.Spec.Unschedulable {
 			fmt.Println("node", node.Name, "is cordoned, uncordoning it")
-			err := nodes.UnCordon(clients, node.Name)
+			err := nodesutils.UnCordon(clients, node.Name)
 
 			if err != nil {
 				return false, err
