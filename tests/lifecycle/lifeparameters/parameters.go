cdd73050069a53a28dbccae2737df538cac86c6e
sobarzan <sobarzan@redhat.com>
2022-03-16 08:41:59 +0200

    Edited parameters.go file

--- tests/lifecycle/lifeparameters/parameters.go
+++ tests/lifecycle/lifeparameters/parameters.go
@@ -22,16 +22,21 @@ var (
 	PreStopCommand         = []string{"/bin/sh", "-c", "killall -0 tail"}
 	LifecycleTestSuiteName = "lifecycle"
 
-	TnfTestCases = []string{"lifecycle-pod-recreation", "lifecycle-pod-scheduling",
-		"lifecycle-deployment-scaling", "lifecycle-pod-termination-grace-period",
-		"lifecycle-pod-owner-type", "lifecycle-container-shutdown", "lifecycle-image-pull-policy",
-		"lifecycle-liveness", "lifecycle-readiness", "lifecycle-statefulset-scaling",
-		"lifecycle-pod-high-availability"}
-
 	TnfShutdownTcName               = "lifecycle-container-shutdown"
 	TnfDeploymentScalingTcName      = "lifecycle-deployment-scaling"
 	TnfTerminationGracePeriodTcName = "lifecycle-pod-termination-grace-period"
 	TnfPodOwnerTypeTcName           = "lifecycle-pod-owner-type"
 	TnfPodRecreationTcName          = "lifecycle-pod-recreation"
 	TnfPodHighAvailabilityTcName    = "lifecycle-pod-high-availability"
+	TnfPodSchedulingTcName          = "lifecycle-pod-scheduling"
+	TnfLivenessTcName               = "lifecycle-liveness"
+	TnfReadinessTcName              = "lifecycle-readiness"
+	TnfStatefulSetScalingTcName     = "lifecycle-statefulset-scaling"
+	TnfImagePullPolicyTcName        = "lifecycle-image-pull-policy"
+
+	TnfTestCases = []string{TnfPodRecreationTcName, TnfPodSchedulingTcName,
+		TnfDeploymentScalingTcName, TnfTerminationGracePeriodTcName,
+		TnfPodOwnerTypeTcName, TnfShutdownTcName, TnfImagePullPolicyTcName,
+		TnfLivenessTcName, TnfReadinessTcName, TnfStatefulSetScalingTcName,
+		TnfPodHighAvailabilityTcName}
 )
