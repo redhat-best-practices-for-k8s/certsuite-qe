076822a4fbedb03b311a8a35389240ca9c7bd9d8
sobarzan <sobarzan@redhat.com>
2022-04-06 12:34:40 +0300

    Added globalHelper/daemonSet

---
+++ tests/globalhelper/daemonSet.go
@@ -0,0 +1,58 @@
+package globalhelper
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/onsi/gomega"
+)
+
+func isDaemonSetReady(operatorNamespace string, daemonSetName string) (bool, error) {
+	daemonSet, err := APIClient.DaemonSets(operatorNamespace).Get(
+		context.Background(),
+		daemonSetName,
+		metav1.GetOptions{},
+	)
+	if err != nil {
+		return false, err
+	}
+
+	if daemonSet.Status.NumberReady > 0 {
+		if daemonSet.Status.NumberUnavailable == 0 {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// CreateAndWaitUntilDaemonSetIsReady creates daemonSet and wait until all  replicas are up and running.
+func CreateAndWaitUntilDaemonSetIsReady(daemonSet *v1.DaemonSet, timeout time.Duration) error {
+	runningDaemonSet, err := APIClient.DaemonSets(daemonSet.Namespace).Create(
+		context.Background(),
+		daemonSet,
+		metav1.CreateOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	Eventually(func() bool {
+		status, err := isDaemonSetReady(runningDaemonSet.Namespace, runningDaemonSet.Name)
+		if err != nil {
+			glog.Fatal(fmt.Sprintf(
+				"daemonset %s is not ready, retry in 5 seconds", runningDaemonSet.Name))
+
+			return false
+		}
+
+		return status
+	}, timeout, 5*time.Second).Should(Equal(true), "DaemonSet is not ready")
+
+	return nil
+}
