944f781b4821f87a1a0d5367be721614fc3fdfbe
Emily Johnson <emjohnso@redhat.com>
2022-08-10 14:33:24 -0400

    Changed RedefineWithHostIpc and RedefineWithAutomountServiceAccountToken  so unnecessary copies of deployments aren't created

--- tests/utils/deployment/deployment.go
+++ tests/utils/deployment/deployment.go
@@ -235,14 +235,10 @@ func RedefineWithPriviledgedContainer(deployment *v1.Deployment) *v1.Deployment 
 	return deployment
 }
 
-func RedefineWithHostIpc(deployment *v1.Deployment, hostIpc bool) *v1.Deployment {
+func RedefineWithHostIpc(deployment *v1.Deployment, hostIpc bool) {
 	deployment.Spec.Template.Spec.HostIPC = hostIpc
-
-	return deployment
 }
 
-func RedefineWithAutomountServiceAccountToken(deployment *v1.Deployment, token bool) *v1.Deployment {
+func RedefineWithAutomountServiceAccountToken(deployment *v1.Deployment, token bool) {
 	deployment.Spec.Template.Spec.AutomountServiceAccountToken = &token
-
-	return deployment
 }
