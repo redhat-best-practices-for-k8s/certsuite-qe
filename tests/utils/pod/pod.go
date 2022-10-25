45be9488806929c98fdfa9c856424ec034857763
Shai Oren <sobarzan@redhat.com>
2022-10-25 13:43:10 +0300

    Automated affinity required pods tc verification

--- tests/utils/pod/pod.go
+++ tests/utils/pod/pod.go
@@ -25,7 +25,16 @@ func DefinePod(podName string, namespace string, image string) *corev1.Pod {
 
 // RedefinePodWithLabel adds label to given pod manifest.
 func RedefinePodWithLabel(pod *corev1.Pod, label map[string]string) {
-	pod.ObjectMeta.Labels = label
+	newMap := make(map[string]string)
+	for k, v := range pod.Labels {
+		newMap[k] = v
+	}
+
+	for k, v := range label {
+		newMap[k] = v
+	}
+
+	pod.Labels = newMap
 }
 
 // RedefineWithReadinessProbe adds readiness probe to given pod manifest.
@@ -95,4 +104,53 @@ func RedefineWithCPUResources(pod *corev1.Pod, limit string, req string) {
 
 func RedefineWithRunTimeClass(pod *corev1.Pod, rtcName string) {
 	pod.Spec.RuntimeClassName = pointer.String(rtcName)
+}
+
+// RedefineWithNodeAffinity redefines pod with nodeAffinity spec.
+func RedefineWithNodeAffinity(pod *corev1.Pod, key string) {
+	pod.Spec.Affinity = &corev1.Affinity{
+		NodeAffinity: &corev1.NodeAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+				NodeSelectorTerms: []corev1.NodeSelectorTerm{
+					{
+						MatchExpressions: []corev1.NodeSelectorRequirement{
+							{
+								Key:      key,
+								Operator: corev1.NodeSelectorOpExists,
+							},
+						},
+					},
+				},
+			},
+		}}
+}
+
+// RedefineWithPodAffinity redefines pod with podAffinity spec.
+func RedefineWithPodAffinity(put *corev1.Pod, label map[string]string) {
+	put.Spec.Affinity = &corev1.Affinity{
+		PodAffinity: &corev1.PodAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
+				{
+					LabelSelector: &metav1.LabelSelector{
+						MatchLabels: label,
+					},
+					TopologyKey: "kubernetes.io/hostname",
+				},
+			},
+		}}
+}
+
+// RedefineWithPodantiAffinity redefines pod with podAntiAffinity spec.
+func RedefineWithPodantiAffinity(put *corev1.Pod, label map[string]string) {
+	put.Spec.Affinity = &corev1.Affinity{
+		PodAntiAffinity: &corev1.PodAntiAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
+				{
+					LabelSelector: &metav1.LabelSelector{
+						MatchLabels: label,
+					},
+					TopologyKey: "kubernetes.io/hostname",
+				},
+			},
+		}}
 }
