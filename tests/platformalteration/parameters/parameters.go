1d59e8b784df24a41c018d59962fcefb765bfca8
Shai Oren <sobarzan@redhat.com>
2022-11-16 16:25:58 +0200

    Add ocp-lifecycle test

--- tests/platformalteration/parameters/parameters.go
+++ tests/platformalteration/parameters/parameters.go
@@ -39,6 +39,7 @@ const (
 	TnfBootParamsName         = "platform-alteration-boot-params"
 	TnfSysctlConfigName       = "platform-alteration-sysctl-config"
 	TnfHugePages2mOnlyName    = "platform-alteration-hugepages-2m-only"
+	TnfOCPLifecycleName       = "platform-alteration-ocp-lifecycle"
 
 	Getenforce    = `chroot /host getenforce`
 	Enforcing     = "Enforcing"
