7ac19f771da08d62d29d16edf9019c529546f464
Brandon Palm <bpalm@redhat.com>
2023-03-13 10:37:49 -0500

    Set install ginkgo version to go.mod version

--- scripts/install-ginkgo.sh
+++ scripts/install-ginkgo.sh
@@ -6,5 +6,5 @@ GINKGO_OLD_VERSION="Ginkgo Version 1.16.5"
 
 if ! which ginkgo || ginkgo version -eq "$GINKGO_OLD_VERSION"; then {  
 	echo "Downloading ginkgo tool"
-	go install github.com/onsi/ginkgo/v2/ginkgo@v2.8.1
+	go install "$(awk '/ginkgo/ {printf "%s/ginkgo@%s", $1, $2}' go.mod)"
 } fi
