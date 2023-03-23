f82d17713ff6b0dbe8e9f3967e97e90b89d69c2e
bnshr <bmandal@redhat.com>
2023-03-23 18:02:24 +0100

    garbage

--- tests/platformalteration/tests/platform_alteration_service_mesh.go
+++ tests/platformalteration/tests/platform_alteration_service_mesh.go
@@ -3,15 +3,14 @@ package tests
 import (
 	"bytes"
 	"context"
-	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
-	"github.com/test-network-function/cnfcert-tests-verification/tests/globalparameters"
 	tshelper "github.com/test-network-function/cnfcert-tests-verification/tests/platformalteration/helper"
 	tsparams "github.com/test-network-function/cnfcert-tests-verification/tests/platformalteration/parameters"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/namespaces"
@@ -24,6 +23,7 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/restmapper"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 
 	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
@@ -32,6 +32,7 @@ import (
 const (
 	istioNamespace = "istio-system"
 	istioCR        = "installed-state"
+	waitingTime    = 2 * time.Minute
 )
 
 /*
@@ -44,8 +45,9 @@ const (
 *
 */
 
+// Not tested yet
 func createServiceMesh(filename string) (bool, error) {
-	bytesInFile, err := ioutil.ReadFile(filename)
+	bytesInFile, err := os.ReadFile(filename)
 
 	if err != nil {
 		log.Fatal(err)
@@ -129,41 +131,84 @@ var _ = Describe("platform-alteration-service-mesh-usage", func() {
 		err := namespaces.Clean(tsparams.PlatformAlterationNamespace, globalhelper.APIClient)
 		Expect(err).ToNot(HaveOccurred())
 
-		createServiceMesh("istio.yaml")
+		// createServiceMesh("istio.yaml")
 	})
 
 	// 56594
 	FIt("istio is installed", func() {
 
-		/*By("Create istio-system namespace")
-		err := namespaces.Create(istioNs, globalhelper.APIClient)
-		Expect(err).ToNot(HaveOccurred())
-
-		gvr := schema.GroupVersionResource{Group: "install.istio.io", Version: "v1alpha1", Resource: "istiooperators"}
-		cr, err := globalhelper.APIClient.DynamicClient.Resource(gvr).Namespace(istioNamespace).Get(context.TODO(), istioCR, metav1.GetOptions{})
-
-		Expect(err).ToNot(HaveOccurred())
-		Expect(cr).NotTo(BeNil())*/
-
 		put := pod.DefinePod(tsparams.TestPodName, tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
 			tsparams.TnfTargetPodLabels)
 		tshelper.AppendIstioContainerToPod(put, globalhelper.Configuration.General.TestImage)
 
-		err := globalhelper.CreateAndWaitUntilPodIsReady(put, 2*time.Minute)
+		err := globalhelper.CreateAndWaitUntilPodIsReady(put, waitingTime)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start platform-alteration-service-mesh-usage test")
+		err = globalhelper.LaunchTests(tsparams.TnfServiceMeshUsageName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		logrus.Error(err)
+		Expect(err).ToNot(HaveOccurred())
+
+		//err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCasePassed)
+		//Expect(err).ToNot(HaveOccurred())
+	})
+
+	// 56596
+	FIt("istio is installed but proxy containers does not exist [negative]", func() {
+
+		By("Check Istio resource exists")
+		gvr := schema.GroupVersionResource{Group: "install.istio.io", Version: "v1alpha1", Resource: "istiooperators"}
+
+		cr, err := globalhelper.APIClient.DynamicClient.Resource(gvr).Namespace(istioNamespace).Get(context.TODO(),
+			"example-istiocontrolplane", metav1.GetOptions{})
+
+		Expect(err).ToNot(HaveOccurred())
+		Expect(cr).NotTo(BeNil())
+
+		put := pod.DefinePod(tsparams.TestPodName, tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
+			tsparams.TnfTargetPodLabels)
+
+		err = globalhelper.CreateAndWaitUntilPodIsReady(put, waitingTime)
 		Expect(err).ToNot(HaveOccurred())
 
 		By("Start platform-alteration-service-mesh-usage test")
 		err = globalhelper.LaunchTests(tsparams.TnfServiceMeshUsageName,
 			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
 
-		logrus.Info(err)
-		Expect(err).ToNot(HaveOccurred())
+		Expect(err).To(HaveOccurred())
 
-		err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCasePassed)
-		Expect(err).ToNot(HaveOccurred())
-
+		//err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseFailed)
+		//Expect(err).ToNot(HaveOccurred())
 	})
 
+	// 56597
+	It("istio is installed but proxy container exist on one pod only [negative]", func() {
+
+		By("Define first pod with instio container")
+		put := pod.DefinePod(tsparams.TestPodName, tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
+			tsparams.TnfTargetPodLabels)
+		tshelper.AppendIstioContainerToPod(put, globalhelper.Configuration.General.TestImage)
+
+		err := globalhelper.CreateAndWaitUntilPodIsReady(put, waitingTime)
+		Expect(err).ToNot(HaveOccurred())
+
+		putb := pod.DefinePod("lifecycle-putb", tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
+			tsparams.TnfTargetPodLabels)
+
+		err = globalhelper.CreateAndWaitUntilPodIsReady(putb, waitingTime)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start platform-alteration-service-mesh-usage test")
+		err = globalhelper.LaunchTests(tsparams.TnfServiceMeshUsageName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred())
+
+		//err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseFailed)
+		//Expect(err).ToNot(HaveOccurred())
+	})
+
+	// TODO: Change this code
 	// 56595
 	It("istio is not installed", func() {
 		By("Start platform-alteration-service-mesh-usage test")
@@ -171,59 +216,7 @@ var _ = Describe("platform-alteration-service-mesh-usage", func() {
 			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
 		Expect(err).ToNot(HaveOccurred())
 
-		err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseSkipped)
-		Expect(err).ToNot(HaveOccurred())
+		//err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseSkipped)
+		//Expect(err).ToNot(HaveOccurred())
 	})
-
-	// 56596
-	It("istio is installed but proxy containers does not exist [negative]", func() {
-		By("Create istio-system namespace")
-		err := namespaces.Create(istioNamespace, globalhelper.APIClient)
-		Expect(err).ToNot(HaveOccurred())
-
-		put := pod.DefinePod(tsparams.TestPodName, tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
-			tsparams.TnfTargetPodLabels)
-
-		err = globalhelper.CreateAndWaitUntilPodIsReady(put, tsparams.WaitingTime)
-		Expect(err).ToNot(HaveOccurred())
-
-		By("Start platform-alteration-service-mesh-usage test")
-		err = globalhelper.LaunchTests(tsparams.TnfServiceMeshUsageName,
-			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
-		Expect(err).To(HaveOccurred())
-
-		err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseFailed)
-		Expect(err).ToNot(HaveOccurred())
-	})
-
-	// 56597
-	It("istio is installed but proxy container exist on one pod only [negative]", func() {
-		By("Create istio-system namespace")
-		err := namespaces.Create(istioNamespace, globalhelper.APIClient)
-		Expect(err).ToNot(HaveOccurred())
-
-		By("Define first pod with instio container")
-		put := pod.DefinePod(tsparams.TestPodName, tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
-			tsparams.TnfTargetPodLabels)
-		tshelper.AppendIstioContainerToPod(put, globalhelper.Configuration.General.TestImage)
-
-		err = globalhelper.CreateAndWaitUntilPodIsReady(put, tsparams.WaitingTime)
-		Expect(err).ToNot(HaveOccurred())
-
-		putb := pod.DefinePod("lifecycle-putb", tsparams.PlatformAlterationNamespace, globalhelper.Configuration.General.TestImage,
-			tsparams.TnfTargetPodLabels)
-
-		err = globalhelper.CreateAndWaitUntilPodIsReady(putb, tsparams.WaitingTime)
-		Expect(err).ToNot(HaveOccurred())
-
-		By("Start platform-alteration-service-mesh-usage test")
-		err = globalhelper.LaunchTests(tsparams.TnfServiceMeshUsageName,
-			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
-		Expect(err).To(HaveOccurred())
-
-		err = globalhelper.ValidateIfReportsAreValid(tsparams.TnfServiceMeshUsageName, globalparameters.TestCaseFailed)
-		Expect(err).ToNot(HaveOccurred())
-
-	})
-
 })
