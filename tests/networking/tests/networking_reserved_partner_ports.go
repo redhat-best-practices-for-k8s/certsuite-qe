4c868ae827375c343f9b10b51ac2bfd1a1f07a1f
Emily Johnson <emjohnso@redhat.com>
2023-03-23 12:19:20 -0400

    Added reserved partner ports tests

---
+++ tests/networking/tests/networking_reserved_partner_ports.go
@@ -0,0 +1,226 @@
+package tests
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalparameters"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/execute"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/namespaces"
+	corev1 "k8s.io/api/core/v1"
+
+	tshelper "github.com/test-network-function/cnfcert-tests-verification/tests/networking/helper"
+	tsparams "github.com/test-network-function/cnfcert-tests-verification/tests/networking/parameters"
+)
+
+var _ = Describe("Networking reserved-partner-ports,", func() {
+
+	execute.BeforeAll(func() {
+
+		By("Clean namespace before all tests")
+		err := namespaces.Clean(tsparams.TestNetworkingNameSpace, globalhelper.APIClient)
+		Expect(err).ToNot(HaveOccurred())
+		err = os.Setenv(globalparameters.PartnerNamespaceEnvVarName, tsparams.TestNetworkingNameSpace)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	BeforeEach(func() {
+
+		By("Clean namespace before each test")
+		err := namespaces.Clean(tsparams.TestNetworkingNameSpace, globalhelper.APIClient)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Remove reports from report directory")
+		err = globalhelper.RemoveContentsFromReportDir()
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61487
+	It("one deployment, one pod, one container not declaring reserved ports", func() {
+
+		By("Define deployment and create it on cluster")
+		err := tshelper.DefineAndCreateDeploymentOnCluster(1)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCasePassed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61505
+	It("one deployment, one pod, one container declaring reserved ports [negative]", func() {
+
+		By("Define and create deployment with container declaring reserved port")
+		err := tshelper.DefineAndCreateDeploymentWithContainerPorts(1, []corev1.ContainerPort{{ContainerPort: 15443}})
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCaseFailed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61506
+	It("one deployment, one pod, two containers, neither declaring reserved ports", func() {
+
+		By("Define deployment with two containers")
+		ports := []corev1.ContainerPort{{ContainerPort: 15002}, {ContainerPort: 15007}}
+		err := tshelper.DefineAndCreateDeploymentWithContainerPorts(2, ports)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCasePassed)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	// 61507
+	It("one deployment, one pod, two containers, one declaring reserved ports [negative]", func() {
+		ports := []corev1.ContainerPort{{ContainerPort: 15020}, {ContainerPort: 15019}}
+
+		By("Define deployment with two containers")
+		err := tshelper.DefineAndCreateDeploymentWithContainerPorts(2, ports)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCaseFailed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61508
+	It("one deployment, one pod not listening on reserved ports", func() {
+
+		By("Define deployment and create it on cluster")
+		err := tshelper.DefineAndCreateDeploymentOnCluster(3)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCasePassed)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	// 61509
+	It("one deployment, one pod listening on reserved ports [negative]", func() {
+
+		By("Define deployment and create it on cluster")
+		err := tshelper.DefineAndCreateDeploymentWithContainerPorts(1, []corev1.ContainerPort{{ContainerPort: 15021}})
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Define service and create it on cluster")
+		err = tshelper.DefineAndCreateServiceOnCluster("test-service", 22624, 22624, false)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCaseFailed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61510
+	It("two deployments, one pod each not listening on reserved ports", func() {
+
+		By("Define first deployment and create it on cluster")
+		err := tshelper.DefineAndCreateDeploymentOnCluster(3)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Define second deployment and create it on cluster")
+		err = tshelper.DefineAndCreateDeployment(tsparams.TestDeploymentBName, 3)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCasePassed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+	// 61517
+	It("two deployments, one pod each, one listening on reserved ports [negative]", func() {
+
+		By("Define first deployment and create it on cluster")
+		err := tshelper.DefineAndCreateDeployment(tsparams.TestDeploymentBName, 3)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Define second deployment and create it on cluster")
+		err = tshelper.DefineAndCreateDeploymentWithContainerPorts(1, []corev1.ContainerPort{{ContainerPort: 15090}})
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Define service and create it on cluster")
+		err = tshelper.DefineAndCreateServiceOnCluster("test-service", 22624, 22624, false)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Start tests")
+		err = globalhelper.LaunchTests(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred())
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TnfOcpReservedPortsUsageTcName,
+			globalparameters.TestCaseFailed)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+
+})
