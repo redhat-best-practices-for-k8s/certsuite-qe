27705ee248511ae7107e10025a92e90f25ed3dcc
Banashri Mandal <bmandal@redhat.com>
2023-03-27 11:36:58 +0200

    Fix : Service Mesh Test (QE) (#278)

--- tests/utils/client/clients.go
+++ tests/utils/client/clients.go
@@ -13,6 +13,7 @@ import (
 	olm "github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/scheme"
 	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/typed/operators/v1alpha1"
 	apiextv1client "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
+	"k8s.io/client-go/dynamic"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	appsv1client "k8s.io/client-go/kubernetes/typed/apps/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -42,6 +43,7 @@ type ClientSet struct {
 	nodev1.NodeV1Interface
 	policyv1.PolicyV1Interface
 	policyv1beta1.PolicyV1beta1Interface
+	DynamicClient dynamic.Interface
 }
 
 // New returns a *ClientBuilder with the given kubeconfig.
@@ -80,6 +82,7 @@ func New(kubeconfig string) *ClientSet {
 	clientSet.NodeV1Interface = nodev1.NewForConfigOrDie(config)
 	clientSet.PolicyV1Interface = policyv1.NewForConfigOrDie(config)
 	clientSet.PolicyV1beta1Interface = policyv1beta1.NewForConfigOrDie(config)
+	clientSet.DynamicClient = dynamic.NewForConfigOrDie(config)
 	clientSet.Config = config
 
 	crScheme := runtime.NewScheme()
