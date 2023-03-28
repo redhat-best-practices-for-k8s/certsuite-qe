a917c71495c7779fa8014a56bde4ef7fdf3ab14a
bnshr <bmandal@redhat.com>
2023-03-28 15:35:02 +0200

    Addressed review comments

--- scripts/install-ginkgo.sh
+++ scripts/install-ginkgo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REQUIRED_GINKGO_VERSION="2.9.2"
+REQUIRED_GINKGO_VERSION="$(grep '/ginkgo' go.mod | cut -d ' ' -f2 | cut -c 2-)"
 INSTALLED_GINKGO_VERSION="$(ginkgo version | { read -r _ _ v _; echo "${v}"; })"
 
 GOPATH="${GOPATH:-/root/go}"
@@ -8,13 +8,13 @@ export PATH=$PATH:$GOPATH/bin
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
-if [ "$(version "${INSTALLED_GINKGO_VERSION}")" -ge "$(version ${REQUIRED_GINKGO_VERSION})" ]; then
+if [ "$(version "${INSTALLED_GINKGO_VERSION}")" -ge "$(version "${REQUIRED_GINKGO_VERSION}")" ]; then
     echo "Version is up to date"
 else
         GINKGO_TMP_DIR=$(mktemp -d)
         cd "$GINKGO_TMP_DIR" || exit
         go mod init tmp
-        GOFLAGS=-mod=mod go install github.com/onsi/ginkgo/v2/ginkgo@v$REQUIRED_GINKGO_VERSION
+        GOFLAGS=-mod=mod go install github.com/onsi/ginkgo/v2/ginkgo@v"$REQUIRED_GINKGO_VERSION"
         rm -rf "$GINKGO_TMP_DIR"
         echo "Downloading ginkgo tool"
         cd - || exit
