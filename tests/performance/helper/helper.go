a81021c07839367b586e51a314248ad5d824d85a
bnshr <bmandal@redhat.com>
2023-04-19 14:30:49 +0200

    Implement rt app with no exec probes

--- tests/performance/helper/helper.go
+++ tests/performance/helper/helper.go
@@ -1,7 +1,16 @@
 package helper
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
 	"k8s.io/utils/pointer"
 
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -51,6 +60,46 @@ func DefineExclusivePod(podName string, namespace string, image string, label ma
 	}
 }
 
+func DefineRtPod(podName string, namespace string, image string, label map[string]string) *corev1.Pod {
+	cpuLimit := "1"
+	memoryLimit := "512Mi"
+	containerCommand := []string{"/bin/bash", "-c", "sleep INF"}
+
+	containerResource := corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(cpuLimit),
+			corev1.ResourceMemory: resource.MustParse(memoryLimit),
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(cpuLimit),
+			corev1.ResourceMemory: resource.MustParse(memoryLimit),
+		},
+	}
+
+	containerSecurityContext := &corev1.SecurityContext{
+		Privileged: pointer.Bool(true),
+		RunAsUser:  pointer.Int64(0),
+	}
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: namespace,
+			Labels:    label},
+		Spec: corev1.PodSpec{
+			TerminationGracePeriodSeconds: pointer.Int64(0),
+			Containers: []corev1.Container{
+				{
+					Name:            "rt-app",
+					Image:           image,
+					Command:         containerCommand,
+					Resources:       containerResource,
+					SecurityContext: containerSecurityContext},
+			},
+		},
+	}
+}
+
 func RedefinePodWithSharedContainer(pod *corev1.Pod, containerIndex int) {
 	totalContainers := len(pod.Spec.Containers)
 	limit := "1"
@@ -66,4 +115,71 @@ func RedefinePodWithSharedContainer(pod *corev1.Pod, containerIndex int) {
 			},
 		}
 	}
+}
+
+func ChangeSchedulingPolicy(pod *corev1.Pod, command string) (outStr, errStr string, err error) {
+	outStr, errStr, err = ExecCommandContainer(pod, command)
+	if err != nil {
+		return "", "", fmt.Errorf("cannot execute command: \" %s \"  on %s err:%w", command, pod.Spec.Containers[0].Name, err)
+	}
+
+	return outStr, errStr, err
+}
+
+// ExecCommand runs command in the pod and returns buffer output.
+func ExecCommandContainer(
+	pod *corev1.Pod, command string) (stdout, stderr string, err error) {
+	commandStr := []string{"sh", "-c", command}
+
+	var buffOut bytes.Buffer
+
+	var buffErr bytes.Buffer
+
+	podName := pod.Name
+	podNamespace := pod.Namespace
+	container := pod.Spec.Containers[0].Name
+
+	logrus.Trace(fmt.Sprintf("execute command on ns=%s, pod=%s container=%s, cmd: %s",
+		podNamespace, podName, container, strings.Join(commandStr, " ")))
+
+	req := globalhelper.APIClient.CoreV1Interface.RESTClient().
+		Post().
+		Namespace(podNamespace).
+		Resource("pods").
+		Name(podName).
+		SubResource("exec").
+		VersionedParams(&corev1.PodExecOptions{
+			Container: container,
+			Command:   commandStr,
+			Stdin:     false,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       false,
+		}, scheme.ParameterCodec)
+
+	exec, err := remotecommand.NewSPDYExecutor(globalhelper.APIClient.Config, "POST", req.URL())
+	if err != nil {
+		logrus.Error(err)
+
+		return stdout, stderr, err
+	}
+
+	err = exec.StreamWithContext(context.TODO(), remotecommand.StreamOptions{
+		Stdout: &buffOut,
+		Stderr: &buffErr,
+	})
+
+	stdout, stderr = buffOut.String(), buffErr.String()
+
+	if err != nil {
+		logrus.Error(err)
+		logrus.Error(req.URL())
+		logrus.Error("command: ", command)
+		logrus.Error("stderr: ", stderr)
+		logrus.Error("stdout: ", stdout)
+
+		return stdout, stderr, err
+	}
+
+	return stdout, stderr, err
 }
