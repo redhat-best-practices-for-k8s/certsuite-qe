127c45ae6464865f61b21753e7a1b68be5d2b6cf
bnshr <bmandal@redhat.com>
2023-04-27 10:52:36 +0200

    Address review comments

--- tests/performance/helper/helper.go
+++ tests/performance/helper/helper.go
@@ -8,12 +8,13 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/remotecommand"
 	"k8s.io/utils/pointer"
 
 	tsparams "github.com/test-network-function/cnfcert-tests-verification/tests/performance/parameters"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -195,4 +196,82 @@ func DeleteRunTimeClass(rtcName string) error {
 	}
 
 	return nil
+}
+
+func ConfigurePrivilegedServiceAccount(namespace string) error {
+	aRole, aRoleBinding, aServiceAccount := getPrivilegedServiceAccountObjects(namespace)
+	// create role
+	_, err := globalhelper.APIClient.RbacV1Interface.Roles(namespace).Create(context.TODO(), &aRole, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("error creating role, err=%w", err)
+	}
+
+	// create rolebinding
+	_, err = globalhelper.APIClient.RbacV1Interface.RoleBindings(namespace).Create(context.TODO(), &aRoleBinding, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("error creating role bindings, err=%w", err)
+	}
+
+	// create service account
+	_, err = globalhelper.APIClient.CoreV1Interface.ServiceAccounts(namespace).Create(context.TODO(),
+		&aServiceAccount, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("error creating service account, err=%w", err)
+	}
+
+	return nil
+}
+
+func getPrivilegedServiceAccountObjects(namespace string) (aRole rbacv1.Role,
+	aRoleBinding rbacv1.RoleBinding, aServiceAccount corev1.ServiceAccount) {
+	aRole = rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Role",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tsparams.PriviledgedRoleName,
+			Namespace: namespace,
+		},
+		Rules: []rbacv1.PolicyRule{{
+			APIGroups: []string{"*"},
+			Resources: []string{"*"},
+			Verbs:     []string{"*"},
+		},
+		},
+	}
+
+	aRoleBinding = rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tsparams.PriviledgedRoleName,
+			Namespace: namespace,
+		},
+		Subjects: []rbacv1.Subject{{
+			Kind:      "ServiceAccount",
+			Name:      tsparams.PriviledgedRoleName,
+			Namespace: namespace,
+		}},
+		RoleRef: rbacv1.RoleRef{
+			Kind:     "Role",
+			Name:     tsparams.PriviledgedRoleName,
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+	}
+
+	aServiceAccount = corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tsparams.PriviledgedRoleName,
+			Namespace: namespace,
+		},
+	}
+
+	return aRole, aRoleBinding, aServiceAccount
 }
--- tests/performance/performance_suite_test.go
+++ tests/performance/performance_suite_test.go
@@ -3,7 +3,6 @@
 package performance
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"runtime"
@@ -16,10 +15,8 @@ import (
 
 	. "github.com/onsi/gomega"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
+	tshelper "github.com/test-network-function/cnfcert-tests-verification/tests/performance/helper"
 	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/namespaces"
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestPerformance(t *testing.T) {
@@ -40,7 +37,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).ToNot(HaveOccurred())
 
 	// Create service account and roles and roles binding
-	err = ConfigurePrivilegedServiceAccount(tsparams.PerformanceNamespace)
+	err = tshelper.ConfigurePrivilegedServiceAccount(tsparams.PerformanceNamespace)
 	Expect(err).ToNot(HaveOccurred())
 
 	By("Define TNF config file")
@@ -66,81 +63,3 @@ var _ = AfterSuite(func() {
 	err = globalhelper.RemoveContentsFromReportDir()
 	Expect(err).ToNot(HaveOccurred())
 })
-
-func ConfigurePrivilegedServiceAccount(namespace string) error {
-	aRole, aRoleBinding, aServiceAccount := getPrivilegedServiceAccountObjects(namespace)
-	// create role
-	_, err := globalhelper.APIClient.RbacV1Interface.Roles(namespace).Create(context.TODO(), &aRole, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("error creating role, err=%w", err)
-	}
-
-	// create rolebinding
-	_, err = globalhelper.APIClient.RbacV1Interface.RoleBindings(namespace).Create(context.TODO(), &aRoleBinding, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("error creating role bindings, err=%w", err)
-	}
-
-	// create service account
-	_, err = globalhelper.APIClient.CoreV1Interface.ServiceAccounts(namespace).Create(context.TODO(),
-		&aServiceAccount, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("error creating service account, err=%w", err)
-	}
-
-	return nil
-}
-
-func getPrivilegedServiceAccountObjects(namespace string) (aRole rbacv1.Role,
-	aRoleBinding rbacv1.RoleBinding, aServiceAccount corev1.ServiceAccount) {
-	aRole = rbacv1.Role{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Role",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      tsparams.PriviledgedRoleName,
-			Namespace: namespace,
-		},
-		Rules: []rbacv1.PolicyRule{{
-			APIGroups: []string{"*"},
-			Resources: []string{"*"},
-			Verbs:     []string{"*"},
-		},
-		},
-	}
-
-	aRoleBinding = rbacv1.RoleBinding{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "RoleBinding",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      tsparams.PriviledgedRoleName,
-			Namespace: namespace,
-		},
-		Subjects: []rbacv1.Subject{{
-			Kind:      "ServiceAccount",
-			Name:      tsparams.PriviledgedRoleName,
-			Namespace: namespace,
-		}},
-		RoleRef: rbacv1.RoleRef{
-			Kind:     "Role",
-			Name:     tsparams.PriviledgedRoleName,
-			APIGroup: "rbac.authorization.k8s.io",
-		},
-	}
-
-	aServiceAccount = corev1.ServiceAccount{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ServiceAccount",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      tsparams.PriviledgedRoleName,
-			Namespace: namespace,
-		},
-	}
-
-	return aRole, aRoleBinding, aServiceAccount
-}
