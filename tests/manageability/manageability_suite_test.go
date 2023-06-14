3ebc69359ff4610db30e733346c4c3f3c3f4be28
Banashri Mandal <bmandal@redhat.com>
2023-06-14 16:07:11 +0200

    Manageability : 100% test coverage (#351)
    * Add test for image tag
    
    * Add test for port name
    
    * Refactor
    
    * Fix linter issue

---
+++ tests/manageability/manageability_suite_test.go
@@ -0,0 +1,60 @@
+//go:build !utest
+
+package performance
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+
+	tsparams "github.com/test-network-function/cnfcert-tests-verification/tests/manageability/parameters"
+	_ "github.com/test-network-function/cnfcert-tests-verification/tests/manageability/tests"
+
+	. "github.com/onsi/gomega"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/namespaces"
+)
+
+func TestPerformance(t *testing.T) {
+	_, currentFile, _, _ := runtime.Caller(0)
+	_ = flag.Lookup("logtostderr").Value.Set("true")
+	_ = flag.Lookup("v").Value.Set(globalhelper.Configuration.General.VerificationLogLevel)
+	_, reporterConfig := GinkgoConfiguration()
+	reporterConfig.JUnitReport = globalhelper.Configuration.GetReportPath(currentFile)
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CNFCert performance tests", reporterConfig)
+}
+
+var _ = BeforeSuite(func() {
+
+	By("Create namespace")
+	err := namespaces.Create(tsparams.ManageabilityNamespace, globalhelper.APIClient)
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Define TNF config file")
+	err = globalhelper.DefineTnfConfig(
+		[]string{tsparams.ManageabilityNamespace},
+		[]string{tsparams.TestPodLabel},
+		[]string{},
+		[]string{})
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+
+	By(fmt.Sprintf("Remove %s namespace", tsparams.ManageabilityNamespace))
+	err := namespaces.DeleteAndWait(
+		globalhelper.APIClient,
+		tsparams.ManageabilityNamespace,
+		tsparams.WaitingTime,
+	)
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Remove reports from reports directory")
+	err = globalhelper.RemoveContentsFromReportDir()
+	Expect(err).ToNot(HaveOccurred())
+})
---
+++ tests/manageability/parameters/parameters.go
@@ -0,0 +1,32 @@
+package parameters
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	WaitingTime = 5 * time.Minute
+)
+
+var (
+	testPodLabelPrefixName = "test-network-function.com/manageability"
+	testPodLabelValue      = "testing"
+	TestPodLabel           = fmt.Sprintf("%s: %s", testPodLabelPrefixName, testPodLabelValue)
+	TestPodName            = "manageability-pod"
+	TnfTargetPodLabels     = map[string]string{
+		testPodLabelPrefixName: testPodLabelValue,
+		"app":                  "test",
+	}
+	TestImageWithValidTag = "httpd:2.4.57"
+	InvalidPortName       = "sftp"
+)
+
+const (
+	TnfTestSuiteName       = "manageability"
+	ManageabilityNamespace = "manageability-ns"
+
+	// TNF test cases names.
+	TnfContainerPortName = "manageability-container-port-name-format"
+	TnfContainerImageTag = "manageability-containers-image-tag"
+)
