6a2ecdf07651f91d81fe08bbfd3cfafc9772741e
David Rabkin <david@rabkin.co.il>
2023-07-11 00:09:29 +0300

    Add shfmt linter, no logic change

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -8,12 +8,11 @@ if which golangci-lint; then
 	echo "golint installed"
 else
 	echo "Downloading golint tool"
-
 	if [[ -z "${GOPATH}" ]]; then
-    DEPLOY_PATH=/tmp/
-  else
-    DEPLOY_PATH=${GOPATH}/bin
-  fi
+		DEPLOY_PATH=/tmp/
+	else
+		DEPLOY_PATH=${GOPATH}/bin
+	fi
 	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${DEPLOY_PATH}" v1.53.3
 fi
 
