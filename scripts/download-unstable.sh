81a3c4b0a3602ac3dd6917feabb40da363f74f6e
Brandon Palm <bpalm@redhat.com>
2023-07-14 08:22:00 -0500

    Adjust based on David's comments

--- scripts/download-unstable.sh
+++ scripts/download-unstable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the image name and tag
-image_name="quay.io/testnetworkfunction/cnf-certification-test"
-image_tag="unstable"
+image_name=quay.io/testnetworkfunction/cnf-certification-test
+image_tag=unstable
 
 # Delete the image if it exists
 if docker image inspect "${image_name}:${image_tag}" >/dev/null; then
@@ -16,5 +16,6 @@ docker pull "${image_name}:${image_tag}"
 if docker image inspect "${image_name}:${image_tag}" >/dev/null; then
 	echo "Docker image ${image_name}:${image_tag} was pulled successfully"
 else
-	echo "Failed to pull Docker image ${image_name}:${image_tag}"
+	echo >&2 "Failed to pull Docker image ${image_name}:${image_tag}"
+	exit 1
 fi
