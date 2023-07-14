a00e38a5b687a262c53dc509568fc2742d02cc5b
Brandon Palm <bpalm@redhat.com>
2023-07-14 08:25:45 -0500

    Download new unstable image (#376)
    * Download new unstable image
    
    * Adjust based on David's comments

---
+++ scripts/download-unstable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the image name and tag
+image_name=quay.io/testnetworkfunction/cnf-certification-test
+image_tag=unstable
+
+# Delete the image if it exists
+if docker image inspect "${image_name}:${image_tag}" >/dev/null; then
+	docker rmi "${image_name}:${image_tag}"
+fi
+
+# Pull the image
+docker pull "${image_name}:${image_tag}"
+
+# Check if the image was pulled successfully
+if docker image inspect "${image_name}:${image_tag}" >/dev/null; then
+	echo "Docker image ${image_name}:${image_tag} was pulled successfully"
+else
+	echo >&2 "Failed to pull Docker image ${image_name}:${image_tag}"
+	exit 1
+fi
