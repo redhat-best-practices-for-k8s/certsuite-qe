ce83c2c2e44e359b948bc33c8a05da1613882a19
aabughosh <88486034+aabughosh@users.noreply.github.com>
2023-07-27 17:43:55 +0300

    Helmtest- add helm chart test (#385)
    * add new test for helm chart certification
    
    * Create tnf_config.yml
    
    * Delete tnf_config.yml
    
    * Update affillated_certification_helm_chart.go
    
    * add installing helm
    
    * use oc function to create and delete ns
    
    * Update affillated_certification_helm_chart.go
    
    * update namesspace create
    
    * Update affillated_certification_helm_chart.go
    
    * Update affillated_certification_helm_chart.go
    
    * Update affillated_certification_helm_chart.go
    
    * update the installation of helm
    
    * update the installation way of helm
    
    * add skip test if helm not installed
    
    * Update affillated_certification_helm_chart.go
    
    * Update affillated_certification_helm_chart.go
    
    * Update tests/affiliatedcertification/tests/affillated_certification_helm_chart.go
    
    Co-authored-by: Emily Johnson <emjohnso@redhat.com>
    
    * Update tests/affiliatedcertification/tests/affillated_certification_helm_chart.go
    
    Co-authored-by: Emily Johnson <emjohnso@redhat.com>
    
    * Update affillated_certification_helm_chart.go
    
    * update capital latter
    
    ---------
    
    Co-authored-by: Emily Johnson <emjohnso@redhat.com>
    Co-authored-by: Banashri Mandal <bmandal@redhat.com>

---
+++ tests/affiliatedcertification/tests/affillated_certification_helm_chart.go
@@ -0,0 +1,111 @@
+package tests
+
+import (
+	"os/exec"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	tsparams "github.com/test-network-function/cnfcert-tests-verification/tests/affiliatedcertification/parameters"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalhelper"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/globalparameters"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/execute"
+	"github.com/test-network-function/cnfcert-tests-verification/tests/utils/namespaces"
+)
+
+var _ = Describe("Affiliated-certification helm chart certification,", func() {
+	execute.BeforeAll(func() {
+		By("Define tnf config file")
+		err := globalhelper.DefineTnfConfig(
+			[]string{tsparams.TestHelmChartCertified},
+			[]string{tsparams.TestPodLabel},
+			[]string{},
+			[]string{})
+		Expect(err).ToNot(HaveOccurred(), "error defining tnf config file")
+	})
+
+	BeforeEach(func() {
+		By("Create namespace")
+		err := namespaces.Create(tsparams.TestHelmChartCertified, globalhelper.GetAPIClient())
+		Expect(err).ToNot(HaveOccurred(), "Error creating namespace")
+	})
+
+	AfterEach(func() {
+		By("Remove the project")
+		err := namespaces.DeleteAndWait(globalhelper.GetAPIClient(), tsparams.TestHelmChartCertified, tsparams.Timeout)
+		Expect(err).ToNot(HaveOccurred(), "Error delete ns affiliated-certification-helmchart-is-certified")
+	})
+
+	It("One helm to test, are certified", func() {
+		By("Check if helm is installed")
+		cmd := exec.Command("/bin/bash", "-c",
+			"helm version")
+		err := cmd.Run()
+		if err != nil {
+			Skip("helm does not exist please install it to run the test.")
+		}
+
+		By("Install a helm chart")
+		cmd = exec.Command("/bin/bash", "-c",
+			"helm repo add openshift-helm-charts https://charts.openshift.io/ "+
+				"&& helm repo update && "+
+				"helm install example-vault1 openshift-helm-charts/hashicorp-vault -n "+tsparams.TestHelmChartCertified)
+		err = cmd.Run()
+		Expect(err).ToNot(HaveOccurred(), "Error installing helm chart")
+
+		By("Start test")
+		err = globalhelper.LaunchTests(
+			tsparams.TestHelmChartCertified,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).ToNot(HaveOccurred(), "Error running "+
+			tsparams.TestHelmChartCertified+" test")
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TestHelmChartCertified,
+			globalparameters.TestCasePassed)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("One helm to test, chart not certified", func() {
+		By("Check if helm is installed")
+		cmd := exec.Command("/bin/bash", "-c",
+			"helm version")
+		err := cmd.Run()
+		if err != nil {
+			Skip("helm does not exist please install it to run the test.")
+		}
+
+		By("Create ns istio-system")
+		err = namespaces.Create("istio-system", globalhelper.GetAPIClient())
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Install a helm chart")
+		cmd = exec.Command("/bin/bash", "-c",
+			"helm repo add istio https://istio-release.storage.googleapis.com/charts "+
+				"&& helm repo update &&"+
+				"helm install istio-base istio/base --set defaultRevision=default -n "+tsparams.TestHelmChartCertified)
+		err = cmd.Run()
+		Expect(err).ToNot(HaveOccurred(), "Error installing helm chart")
+
+		By("Start test")
+		err = globalhelper.LaunchTests(
+			tsparams.TestHelmChartCertified,
+			globalhelper.ConvertSpecNameToFileName(CurrentSpecReport().FullText()))
+		Expect(err).To(HaveOccurred(), "Error running "+
+			tsparams.TestHelmChartCertified+" test")
+
+		By("Verify test case status in Junit and Claim reports")
+		err = globalhelper.ValidateIfReportsAreValid(
+			tsparams.TestHelmChartCertified,
+			globalparameters.TestCaseFailed)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Remove the istio-system ns and istio chart")
+		cmd = exec.Command("/bin/bash", "-c", // uinstall the chart
+			"helm uninstall istio-base -n "+tsparams.TestHelmChartCertified)
+		err = cmd.Run()
+		Expect(err).ToNot(HaveOccurred(), "Error installing helm chart")
+		err = namespaces.Clean("istio-system", globalhelper.GetAPIClient())
+		Expect(err).ToNot(HaveOccurred(), "Error delete ns istio-system")
+	})
+})
