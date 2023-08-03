65fdb381de31a74892eb2c241764054b330f2222
Emily Johnson <emjohnso@redhat.com>
2023-08-03 16:22:46 -0400

    Operator install source tests (#414)
    * initial setup for operator suite,
    operator-install-source tests
    
    * added implementation of
    positive operator install source case, necessary utilities
    
    * Added additional operators,
    ability to delete individual subscriptions;
    implemented negative and multiple-operator test cases
    
    * removed unused function
    
    * linting fixes

---
+++ tests/globalhelper/subscription.go
@@ -0,0 +1,37 @@
+package globalhelper
+
+import (
+	"context"
+	"fmt"
+
+	testclient "github.com/test-network-function/cnfcert-tests-verification/tests/utils/client"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+)
+
+func DeleteSubscription(namespace string, subscriptionName string, clientSet *testclient.ClientSet) error {
+	subscription, err := clientSet.Subscriptions(namespace).Get(context.TODO(),
+		subscriptionName,
+		metav1.GetOptions{})
+
+	if k8serrors.IsNotFound(err) {
+		return nil
+	}
+
+	if subscription == nil {
+		return err
+	}
+
+	err = clientSet.Subscriptions(namespace).Delete(context.TODO(),
+		subscriptionName,
+		metav1.DeleteOptions{
+			GracePeriodSeconds: pointer.Int64(0),
+		})
+
+	if err != nil {
+		return fmt.Errorf("failed to delete subscription %w", err)
+	}
+
+	return nil
+}
