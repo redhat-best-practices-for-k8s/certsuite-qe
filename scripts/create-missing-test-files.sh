c49fc5644d10705558f739ef6148eca5a7ed667d
Brandon Palm <bpalm@redhat.com>
2023-10-09 10:18:12 -0500

    Address comments

--- scripts/create-missing-test-files.sh
+++ scripts/create-missing-test-files.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2043
-for folder in tests; do
-	echo "./$folder"
 
-	FILE_NAMES=$(find ./$folder -name "*.go" | grep -v "_test.go" | grep -v "_moq.go" | grep -v "ginkgo" | sed 's/.go//g')
+file_names=$(find ./tests -name "*.go" | grep -v "_test.go" | grep -v "_moq.go" | grep -v "ginkgo" | sed 's/.go//g')
 
-	# if a filename_test.go does not exist, create it
-	for FILE_NAME in $FILE_NAMES; do
-		if [ ! -f "$FILE_NAME"_test.go ]; then
-			touch "$FILE_NAME"_test.go
-
-			PACKAGE_NAME=$(grep -m 1 "package" "$FILE_NAME".go | awk '{print $2}')
-			echo "package $PACKAGE_NAME" >>"$FILE_NAME"_test.go
-		fi
-	done
+# if a filename_test.go does not exist, create it
+for file_name in $file_names; do
+	if [ ! -f "$file_name"_test.go ]; then
+		package_name=$(grep -m 1 "package" "$file_name".go | awk '{print $2}')
+		echo "package $package_name" >>"$file_name"_test.go
+	fi
 done
-rm -f {}_test.go
