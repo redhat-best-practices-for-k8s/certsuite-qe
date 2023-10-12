901928a7ca3745ff74eaf8eb4e6666292a742bf4
David Rabkin <david@rabkin.co.il>
2023-10-12 19:02:53 +0300

    More elegant shell

--- scripts/create-missing-test-files.sh
+++ scripts/create-missing-test-files.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-file_names=$(find ./tests -name "*.go" | grep -v "_test.go" | grep -v "_moq.go" | grep -v "ginkgo" | sed 's/.go//g')
+# Lists only files with package name.
+file_names=$(
+	find ./tests \
+		-name "*.go" \
+		! -name "*_test.go" \
+		! -name "*_moq.go" \
+		! -name "*ginkgo*" |
+		sed 's/.go//g'
+)
 
-# if a filename_test.go does not exist, create it
+# Creates the file if it doesn't exist.
+skp=0
+add=0
 for file_name in $file_names; do
-	if [ ! -f "$file_name"_test.go ]; then
-		package_name=$(grep -m 1 "package" "$file_name".go | awk '{print $2}')
-		echo "package $package_name" >>"$file_name"_test.go
-	fi
+	[ -f "$file_name"_test.go ] && {
+		skp=$((skp + 1))
+		continue
+	}
+	package_name=$(grep -m 1 package "$file_name".go | awk '{print $2}')
+	echo "package $package_name" >>"$file_name"_test.go
+	add=$((add + 1))
 done
+printf >&2 'Added %d and skipped %d file(s).\n' $add $skp
