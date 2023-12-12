11ac4326b6cd1449c8f696055135582748b91ef5
David Elie-Dit-Cosaque <deliedit@redhat.com>
2023-12-12 17:59:24 -0600

    Add container engine variable

--- tests/utils/container/container.go
+++ tests/utils/container/container.go
@@ -2,49 +2,9 @@ package container
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"path"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 )
-
-// SelectEngine check what container engine is present on the machine.
-func SelectEngine() (string, error) {
-	for _, containerEngine := range []string{"docker", "podman"} {
-		containerEngineCMD := exec.Command(containerEngine)
-		directoryName, _ := path.Split(containerEngineCMD.Path)
-
-		if directoryName != "" {
-			if strings.Contains(containerEngineCMD.String(), "docker") {
-				err := validateDockerDaemonRunning()
-				if err != nil {
-					return "", err
-				}
-			}
-
-			return containerEngine, nil
-		}
-	}
-
-	return "nil", fmt.Errorf("no container Engine present on host machine")
-}
-
-func validateDockerDaemonRunning() error {
-	// To make it run on MacOs or Windows
-	if _, isNonLinuxEnv := os.LookupEnv("NON_LINUX_ENV"); isNonLinuxEnv {
-		return nil
-	}
-
-	isDaemonRunning := exec.Command("systemctl", "is-active", "--quiet", "docker")
-
-	if isDaemonRunning.Run() != nil {
-		return fmt.Errorf("docker daemon is not active on host")
-	}
-
-	return nil
-}
 
 func CreateContainerSpecsFromContainerPorts(ports []corev1.ContainerPort, image, name string) []corev1.Container {
 	numContainers := len(ports)
