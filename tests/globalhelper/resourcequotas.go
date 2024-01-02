5fc14488c05420f12975c3e7aecb36f1fc9cb793
Brandon Palm <bpalm@redhat.com>
2024-01-02 15:39:23 -0600

    Add more unit tests

--- tests/globalhelper/resourcequotas.go
+++ tests/globalhelper/resourcequotas.go
@@ -2,8 +2,11 @@ package globalhelper
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/golang/glog"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -23,6 +26,12 @@ func createResourceQuota(client kubernetes.Interface, quota *corev1.ResourceQuot
 	}
 
 	_, err1 := client.CoreV1().ResourceQuotas(quota.Namespace).Create(context.TODO(), quota, metav1.CreateOptions{})
+
+	if k8serrors.IsAlreadyExists(err1) {
+		glog.V(5).Info(fmt.Sprintf("resource quota %s already exists", quota.Name))
+
+		return nil
+	}
 
 	return err1
 }
--- tests/globalhelper/runtimeclass_test.go
+++ tests/globalhelper/runtimeclass_test.go
@@ -1,1 +1,87 @@
 package globalhelper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	nodev1 "k8s.io/api/node/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestIsRTCCreated(t *testing.T) {
+	testCases := []struct {
+		rtcAlreadyExists bool
+	}{
+		{rtcAlreadyExists: false},
+		{rtcAlreadyExists: true},
+	}
+
+	for _, testCase := range testCases {
+		var runtimeObjects []runtime.Object
+
+		if testCase.rtcAlreadyExists {
+			// Create a fake runtime class object
+			runtimeObjects = append(runtimeObjects, &nodev1.RuntimeClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "testRTC",
+					UID:  "testUID",
+				},
+			})
+		}
+
+		// Create a fake clientset
+		client := k8sfake.NewSimpleClientset(runtimeObjects...)
+		isCreated, err := isRtcCreated(client, &nodev1.RuntimeClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "testRTC",
+				UID:  "testUID",
+			},
+		})
+
+		assert.Equal(t, testCase.rtcAlreadyExists, isCreated)
+
+		if testCase.rtcAlreadyExists {
+			assert.Nil(t, err)
+		} else {
+			assert.True(t, k8serrors.IsNotFound(err))
+		}
+	}
+}
+
+func TestIsRtcDeleted(t *testing.T) {
+	testCases := []struct {
+		rtcAlreadyDeleted bool
+	}{
+		{rtcAlreadyDeleted: false},
+		{rtcAlreadyDeleted: true},
+	}
+
+	for _, testCase := range testCases {
+		var runtimeObjects []runtime.Object
+
+		if !testCase.rtcAlreadyDeleted {
+			// Create a fake runtime class object
+			runtimeObjects = append(runtimeObjects, &nodev1.RuntimeClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "testRTC",
+					UID:  "testUID",
+				},
+			})
+		}
+
+		// Create a fake clientset
+		client := k8sfake.NewSimpleClientset(runtimeObjects...)
+		isDeleted, err := isRtcDeleted(client, &nodev1.RuntimeClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "testRTC",
+				UID:  "testUID",
+			},
+		})
+
+		assert.Equal(t, testCase.rtcAlreadyDeleted, isDeleted)
+		assert.Nil(t, err)
+	}
+}
