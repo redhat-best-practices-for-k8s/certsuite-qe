a1dcf563bec8909f8d235ac920e413e6200800c5
Brandon Palm <bpalm@redhat.com>
2024-02-14 11:28:31 -0600

    Update Istio to v1.20.3

--- tests/platformalteration/tests/platform_alteration_service_mesh.go
+++ tests/platformalteration/tests/platform_alteration_service_mesh.go
@@ -32,8 +32,8 @@ var _ = Describe("platform-alteration-service-mesh-usage-installed", Ordered, fu
 		if _, exists := os.LookupEnv("NON_LINUX_ENV"); !exists {
 			By("Install istio")
 			//nolint:goconst
-			cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.2 sh - "+
-				"&& istio-1.20.2/bin/istioctl install --set profile=demo -y")
+			cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.3 sh - "+
+				"&& istio-1.20.3/bin/istioctl install --set profile=demo -y")
 			err := cmd.Run()
 			Expect(err).ToNot(HaveOccurred(), "Error installing istio")
 		}
@@ -42,8 +42,8 @@ var _ = Describe("platform-alteration-service-mesh-usage-installed", Ordered, fu
 	AfterAll(func() {
 		if _, exists := os.LookupEnv("NON_LINUX_ENV"); !exists {
 			By("Uninstall istio")
-			cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.2 sh - "+
-				"&& istio-1.20.2/bin/istioctl uninstall -y --purge")
+			cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.3 sh - "+
+				"&& istio-1.20.3/bin/istioctl uninstall -y --purge")
 			err := cmd.Run()
 			Expect(err).ToNot(HaveOccurred(), "Error uninstalling istio")
 		}
@@ -170,8 +170,8 @@ var _ = Describe("platform-alteration-service-mesh-usage-uninstalled", Serial, f
 		if err == nil {
 			By("Uninstall istio")
 			if _, exists := os.LookupEnv("NON_LINUX_ENV"); !exists {
-				cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.2 sh - "+
-					"&& istio-1.20.2/bin/istioctl uninstall -y --purge")
+				cmd := exec.Command("/bin/bash", "-c", "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.3 sh - "+
+					"&& istio-1.20.3/bin/istioctl uninstall -y --purge")
 				err := cmd.Run()
 				Expect(err).ToNot(HaveOccurred(), "Error uninstalling istio")
 			}
