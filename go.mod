06d314b077c8337e2d764ca0c5dfadde1ff669aa
Brandon Palm <bpalm@redhat.com>
2024-04-11 16:16:14 -0500

    Update Go to v1.22.2 (#724)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/test-network-function/cnfcert-tests-verification
 
-go 1.22.1
+go 1.22.2
 
 require (
 	github.com/golang/glog v1.2.1
--- scripts/install-latest-go.sh
+++ scripts/install-latest-go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REQUIRED_GO_VERSION=1.22.1
+REQUIRED_GO_VERSION=1.22.2
 INSTALLED_GO_VERSION="$(
 	go version | {
 		read -r _ _ v _
