a91a977149b4a0effb6ff5f392d11a0178973730
Brandon Palm <bpalm@redhat.com>
2024-04-22 10:40:08 -0500

    Add openshift-marketplace cleanup script (#738)

---
+++ scripts/clean-marketplace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set the namespace
+NAMESPACE="openshift-marketplace"
+
+# Get the list of pods in the namespace
+PODS=$(kubectl get pods -n "$NAMESPACE" -o name)
+
+# Delete each pod
+for pod in $PODS; do
+	kubectl delete "$pod" -n "$NAMESPACE"
+done
