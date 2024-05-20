b3472c7de9826e08f5d34850d9c613911d3bcd11
Brandon Palm <bpalm@redhat.com>
2024-05-20 13:47:50 -0500

    Automatically add community-operators catalog source (#767)

--- tests/globalhelper/catalogsources.go
+++ tests/globalhelper/catalogsources.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
 	v1alpha1typed "github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/typed/operators/v1alpha1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -50,4 +52,64 @@ func contains(s []string, e string) bool {
 	}
 
 	return false
+}
+
+func createCatalogSource(name, url string) error {
+	return createCatalogSourceWithClient(GetAPIClient().OperatorsV1alpha1Interface, name, url)
+}
+
+func createCatalogSourceWithClient(opclient v1alpha1typed.OperatorsV1alpha1Interface, name, url string) error {
+	_, err := opclient.CatalogSources(CatalogSourceNamespace).Create(context.Background(), &v1alpha1.CatalogSource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "openshift-marketplace",
+		},
+		Spec: v1alpha1.CatalogSourceSpec{
+			SourceType:  "grpc",
+			Image:       url,
+			Publisher:   "Red Hat",
+			DisplayName: name,
+		},
+	}, metav1.CreateOptions{})
+
+	if err != nil && !k8serrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	return nil
+}
+
+func CreateCommunityOperatorsCatalogSource() error {
+	communityOperatorIndex414 := "registry.redhat.io/redhat/community-operator-index:v4.14"
+	communityOperatorIndex415 := "registry.redhat.io/redhat/community-operator-index:v4.15"
+
+	// determine which index to use based on ocp version
+	ocpVersion, err := GetClusterVersion()
+	if err != nil {
+		return err
+	}
+
+	// strip off the patch version (e.g. 4.14.0 -> 4.14)
+	majorMinor := ocpVersion[:4]
+
+	// Note: Update this when new OCP versions are released and new community operator indexes are available
+	switch majorMinor {
+	case "4.14":
+		return createCatalogSource("community-operators", communityOperatorIndex414)
+	case "4.15":
+		return createCatalogSource("community-operators", communityOperatorIndex415)
+	default:
+		return fmt.Errorf("unsupported ocp version %s", ocpVersion)
+	}
+}
+
+func GetClusterVersion() (string, error) {
+	clusterVersion, err := GetAPIClient().ClusterVersionInterface.
+		Get(context.Background(), "version", metav1.GetOptions{})
+
+	if err != nil {
+		return "", err
+	}
+
+	return clusterVersion.Status.Desired.Version, nil
 }
