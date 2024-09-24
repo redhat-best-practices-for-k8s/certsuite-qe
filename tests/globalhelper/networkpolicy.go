8a4842cfa2642f4c003dd6150aacbbf387d8fce3
Brandon Palm <bpalm@redhat.com>
2024-09-24 15:22:12 -0500

    Add networkPolicy pkg from eco-goinfra

--- tests/globalhelper/networkpolicy.go
+++ tests/globalhelper/networkpolicy.go
@@ -2,50 +2,20 @@ package globalhelper
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	"github.com/golang/glog"
 	networkingv1 "k8s.io/api/networking/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	. "github.com/onsi/gomega"
+	egiClients "github.com/openshift-kni/eco-goinfra/pkg/clients"
 )
 
-func CreateAndWaitUntilNetworkPolicyIsReady(networkPolicy *networkingv1.NetworkPolicy, timeout time.Duration) error {
-	policy, err := GetAPIClient().NetworkPolicies(networkPolicy.Namespace).Create(
-		context.TODO(), networkPolicy, metav1.CreateOptions{})
-
-	if k8serrors.IsAlreadyExists(err) {
-		glog.V(5).Info(fmt.Sprintf("Network Policy %s already exists", networkPolicy.Name))
-	} else if err != nil {
-		return fmt.Errorf("failed to create Network Policy %q (ns %s): %w", networkPolicy.Name, networkPolicy.Namespace, err)
-	}
-
-	Eventually(func() bool {
-		status, err := doesNetworkPolicyExist(policy.Namespace, policy.Name)
-		if err != nil {
-			glog.Errorf(
-				"Network Policy %s is not ready.", policy.Name)
-
-			return false
-		}
-
-		return status
-	}, timeout, retryInterval*time.Second).Should(Equal(true), "Network Policy is not ready")
-
-	return nil
-}
-
-func doesNetworkPolicyExist(namespace, name string) (bool, error) {
-	if _, err := GetAPIClient().NetworkPolicies(namespace).Get(
+func CreateNetworkPolicy(networkPolicy *networkingv1.NetworkPolicy, timeout time.Duration) error {
+	client := egiClients.New("")
+	_, err := client.NetworkPolicies(networkPolicy.Namespace).Create(
 		context.TODO(),
-		name,
-		metav1.GetOptions{},
-	); err != nil {
-		return false, err
-	}
+		networkPolicy,
+		metav1.CreateOptions{})
 
-	return true, nil
+	return err
 }
