5e305fefc865b9ece910e2ec5821d7db47f934dc
Brandon Palm <bpalm@redhat.com>
2025-10-13 11:14:16 -0500

    Version updates; KinD and GolangCI

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -13,7 +13,7 @@ else
 	else
 		DEPLOY_PATH=${GOPATH}/bin
 	fi
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${DEPLOY_PATH}" v2.0.1
+	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${DEPLOY_PATH}" v2.5.0
 fi
 
 PATH=${PATH}:${DEPLOY_PATH} golangci-lint run -v
